837daf0392b055b0e7eea3142cdfa4cce0ffbf3a
pratyush8470 <pratyushbhatiya106@gmail.com>
2024-10-09 15:59:30 +0530

    movie management

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/public", express.static(path.join(__dirname,"/public")));
+
 
 app.use("/", routes);
 app.use(express.static(Path))
